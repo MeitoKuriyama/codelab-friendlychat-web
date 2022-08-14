ad4456d475a502d4a70983e9e26b74739e807511
MeitoKuriyama <meito.kuri@gmail.com>
2022-08-14 22:25:01 +0900

    7まで終了
    次は8から

--- web-start/src/firebase-config.js
+++ web-start/src/firebase-config.js
@@ -1,6 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCH6F5j4S3d4Vi2vyu1fLo0ogR1yxaDMc",
+  authDomain: "friendlychat-aa95f.firebaseapp.com",
+  projectId: "friendlychat-aa95f",
+  storageBucket: "friendlychat-aa95f.appspot.com",
+  messagingSenderId: "617891499587",
+  appId: "1:617891499587:web:c5eeb733be07b852828ef6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 /**
  * To find your Firebase config object:
- * 
+ *
  * 1. Go to your [Project settings in the Firebase console](https://console.firebase.google.com/project/_/settings/general/)
  * 2. In the "Your apps" card, select the nickname of the app for which you need a config object.
  * 3. Select Config from the Firebase SDK snippet pane.
@@ -8,6 +25,12 @@
  */
 const config = {
   /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+  apiKey: "AIzaSyCCH6F5j4S3d4Vi2vyu1fLo0ogR1yxaDMc",
+  authDomain: "friendlychat-aa95f.firebaseapp.com",
+  projectId: "friendlychat-aa95f",
+  storageBucket: "friendlychat-aa95f.appspot.com",
+  messagingSenderId: "617891499587",
+  appId: "1:617891499587:web:c5eeb733be07b852828ef6"
 };
 
 export function getFirebaseConfig() {
